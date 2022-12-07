0cbeca77adfda883d475b2b73d65b129e9b57492
CCMatson <catherine.matson@gmail.com>
2022-12-06 18:37:02 -0800

    adding a button eventto add list items and then clear the input

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-const inputElement = document.getElementById('list acceptor')
-const btnElement = document.getElementById('submit button')
+const inputElement = document.getElementById('list-acceptor')
+const btnElement = document.getElementById('submit-button')
 const ulElement = document.getElementById('todo-list')
+
+//const btn = document.querySelector('#submit button')
+
+btnElement.addEventListener('click', function(evt){
+  //create new li element
+  const li = document.createElement('li')
+  //sets the li content to the value at input
+  li.textContent = inputElement.value
+  // adds to the li content to the ul area
+  document.querySelector('ul').appendChild(li)
+  inputElement.value = ''
+  //console.log(evt.target)
+})
+
+
+
+// const btn = document.querySelector('button')
+// btn.addEventListener('click', function(evt) {
+//   // testing!
+//   console.log(evt)
+// })
